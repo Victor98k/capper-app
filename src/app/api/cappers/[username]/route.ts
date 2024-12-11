26f9576c4e9a682f1c2653df7bffcf62dc660a08
Victor98k <Victorgustav98@gmail.com>
2024-12-11 19:08:19 +0100

    deploy error removed dublicate file for route.

--- src/app/api/cappers/[username]/route.ts
+++ src/app/api/cappers/[username]/route.ts
@@ -1,8 +1,9 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { type NextRequest } from "next/server";
 
 export async function GET(
-  _req: Request,
+  request: NextRequest,
   { params }: { params: { username: string } }
 ) {
   try {
