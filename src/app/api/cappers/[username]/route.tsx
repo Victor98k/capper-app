d42af00b4aa385714ae7c6f4b27046506b373953
Victor98k <Victorgustav98@gmail.com>
2024-12-14 12:44:38 +0100

    deploy err, await added for params.

--- src/app/api/cappers/[username]/route.tsx
+++ src/app/api/cappers/[username]/route.tsx
@@ -6,7 +6,7 @@ export async function GET(
   request: NextRequest,
   { params }: { params: { username: string } }
 ) {
-  const username = params.username;
+  const username = await params.username;
 
   try {
     const capper = await prisma.capper.findFirst({
