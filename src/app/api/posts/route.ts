6e19c216a10f872c72442e7edc755e639516d61f
Victor98k <Victorgustav98@gmail.com>
2024-12-27 01:11:36 +0100

    is verified removed for deploy.

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -72,7 +72,7 @@ export async function GET(req: Request) {
         lastName: post.capper.user.lastName,
         username: post.capper.user.username,
         imageUrl: post.capper.user.imageUrl,
-        isVerified: post.capper.user.isVerified || false,
+        // isVerified: post.capper.user.isVerified || false,
       },
     }));
 
