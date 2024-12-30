991424ed326d994b3ebd273df013ae4631befd92
Victor98k <Victorgustav98@gmail.com>
2024-12-30 22:24:36 +0100

    deploy err with next auth try to fix.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,54 +1,54 @@
-// import { getServerSession } from "@auth/nextjs";
-// import type { NextAuthConfig } from "next-auth";
-// import type { JWT } from "next-auth/jwt";
-// import type { Session } from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
-// import { prisma } from "./prisma";
+import { getServerSession } from "next-auth/next";
+import type { NextAuthConfig } from "next-auth";
+import type { JWT } from "next-auth/jwt";
+import type { Session } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { prisma } from "./prisma";
 
-// export const authOptions: NextAuthConfig = {
-//   providers: [
-//     CredentialsProvider({
-//       name: "Credentials",
-//       credentials: {
-//         email: { label: "Email", type: "text" },
-//         password: { label: "Password", type: "password" },
-//       },
-//       async authorize(credentials) {
-//         if (!credentials?.email || !credentials?.password) {
-//           return null;
-//         }
+export const authOptions: NextAuthConfig = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          return null;
+        }
 
-//         const user = await prisma.user.findUnique({
-//           where: {
-//             email: credentials.email as string,
-//           },
-//         });
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email as string,
+          },
+        });
 
-//         if (!user) {
-//           return null;
-//         }
+        if (!user) {
+          return null;
+        }
 
-//         // Add your password verification logic here
+        // Add your password verification logic here
 
-//         return {
-//           id: user.id,
-//           email: user.email,
-//           name: user.firstName + " " + user.lastName,
-//         };
-//       },
-//     }),
-//   ],
-//   session: {
-//     strategy: "jwt",
-//   },
-//   callbacks: {
-//     async session({ session, token }: { session: Session; token: JWT }) {
-//       if (token && session.user) {
-//         session.user.id = token.sub || "";
-//       }
-//       return session;
-//     },
-//   },
-// };
+        return {
+          id: user.id,
+          email: user.email,
+          name: user.firstName + " " + user.lastName,
+        };
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async session({ session, token }: { session: Session; token: JWT }) {
+      if (token && session.user) {
+        session.user.id = token.sub || "";
+      }
+      return session;
+    },
+  },
+};
 
-// export const auth = () => getServerSession(authOptions);
+export const auth = () => getServerSession(authOptions);
