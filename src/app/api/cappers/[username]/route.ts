8fb0576da99e06c476dd5dfbbce0276347d11e75
Victor98k <Victorgustav98@gmail.com>
2025-01-19 14:52:21 +0100

    trying to resolve deploy err.

--- src/app/api/cappers/[username]/route.ts
+++ src/app/api/cappers/[username]/route.ts
@@ -1,6 +1,5 @@
 import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
-import { type NextRequest } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import { stripe } from "@/lib/stripe";
 
 export async function GET(
