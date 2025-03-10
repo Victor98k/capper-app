b4ec1da04a4e0c92e4aed466be5a6c376358ead6
Victor98k <Victorgustav98@gmail.com>
2025-03-11 00:00:58 +0100

    key push again.

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,9 +1,7 @@
 import Stripe from "stripe";
 
-if (!process.env.STRIPE_WEBHOOK_SECRET_LIVE) {
-  throw new Error(
-    "STRIPE_WEBHOOK_SECRET_LIVE is missing in environment variables"
-  );
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error("STRIPE_SECRET_KEY is missing in environment variables");
 }
 
 // Add retry logic for rate limits
@@ -44,7 +42,7 @@ const stripeWithRetry = (stripeInstance: Stripe) => {
 };
 
 export const stripe = stripeWithRetry(
-  new Stripe(process.env.STRIPE_WEBHOOK_SECRET_LIVE, {
+  new Stripe(process.env.STRIPE_SECRET_KEY, {
     apiVersion: "2024-12-18.acacia", // Use the latest API version
     typescript: true,
   })
