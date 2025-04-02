4ee0189eb299218a901c4715c7f2bea555965625
Victor98k <Victorgustav98@gmail.com>
2025-04-02 07:58:19 +0200

    remove api folder from sitemap.

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   siteUrl: process.env.NEXT_PUBLIC_APP_URL || "https://www.app.cappersports.co",
   generateRobotsTxt: true, // (optional) you can disable robots.txt generation
-  exclude: ["/api/*", "/admin/*"],
+  exclude: ["/api/*", "/api/**"], // Exclude all API routes
   generateIndexSitemap: false,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: "*",
+        disallow: ["/api/"], // Explicitly disallow crawling of API routes
+      },
+    ],
+  },
 };
