8e1bc338ffd1ae6472f20b57f027817bcf0d3ff5
Victor98k <Victorgustav98@gmail.com>
2025-04-03 22:53:58 +0200

    added logo to signup.

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,10 +1,18 @@
 import Signup from "@/components/signup";
+import capperLogo from "@/images/Cappers Logga (1).svg";
 
 function SignUpPage() {
   return (
-    <>
+    <div className="min-h-screen flex flex-col items-center pt-8 md:pt-12">
+      <div className="mb-8">
+        <img
+          src={capperLogo.src}
+          alt="Cappers Logo"
+          className="h-16 md:h-20 lg:h-24"
+        />
+      </div>
       <Signup />
-    </>
+    </div>
   );
 }
 
